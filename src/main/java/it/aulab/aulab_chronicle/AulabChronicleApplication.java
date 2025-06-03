1fcd03af9426b362095dc9c1093410bb3ba8e9bf
Davide Nicolò Mozzillo <mozzillodavide@gmail.com>
2025-06-03 19:57:06 +0200

    creazione progetto e inserimento dipendenze

---
+++ src/main/java/it/aulab/aulab_chronicle/AulabChronicleApplication.java
@@ -0,0 +1,13 @@
+package it.aulab.aulab_chronicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulabChronicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulabChronicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/aulab/aulab_chronicle/AulabChronicleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aulab.aulab_chronicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulabChronicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
