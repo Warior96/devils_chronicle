68d256a32fa7b5ca09755451c587a0870d3dede3
Davide Nicolò Mozzillo <mozzillodavide@gmail.com>
2025-06-03 20:36:08 +0200

    creati models user role

---
+++ src/main/java/it/aulab/aulab_chronicle/models/User.java
@@ -0,0 +1,36 @@
+package it.aulab.aulab_chronicle.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = {
+            @JoinColumn(name = "USER_ID", referencedColumnName = "ID") }, inverseJoinColumns = {
+                    @JoinColumn(name = "ROLE_ID", referencedColumnName = "ID") })
+    private List<Role> roles = new ArrayList<>();
+
+}
