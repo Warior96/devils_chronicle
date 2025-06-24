3252ff7b129163ce714548af422da1276d8cc8bb
Davide Nicolò Mozzillo <mozzillodavide@gmail.com>
2025-06-24 20:50:50 +0200

    aggiunta anteprima covere gallery in create article

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,3 +4,39 @@ console.log("script loaded");
 function goBack() {
     window.history.back();
 }
+
+// preview cover articolo
+const coverImage = document.querySelector("#image");
+const coverPreview = document.querySelector("#coverPreview");
+let oldPreviewUrl = null;
+
+coverImage.addEventListener("change", () => {
+    if (oldPreviewUrl) {
+        URL.revokeObjectURL(oldPreviewUrl);
+    }
+    if (coverImage.files && coverImage.files[0]) {
+        oldPreviewUrl = URL.createObjectURL(coverImage.files[0]);
+        coverPreview.src = oldPreviewUrl;
+        coverPreview.classList.remove("d-none");
+    }
+});
+
+
+// preview gallery articolo
+const galleryInput = document.querySelector("#galleryImages");
+const galleryPreview = document.querySelector("#galleryPreview");
+
+galleryInput.addEventListener("change", () => {
+    galleryPreview.innerHTML = "";
+
+    const files = Array.from(galleryInput.files);
+
+    files.forEach(file => {
+        const img = document.createElement("img");
+        img.src = URL.createObjectURL(file);
+        img.classList.add("col-3", "mt-3", "px-2");
+        img.onload = () => URL.revokeObjectURL(img.src);
+
+        galleryPreview.appendChild(img);
+    });
+});
